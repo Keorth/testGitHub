294f9dfb73571fbb623e4c085dfea89933362ed0
chaillou <chaillou@hp60.dev.afpa>
2019-03-25 14:45:46 +0100

    add function hello()

--- src/testgithub/Main.java
+++ src/testgithub/Main.java
@@ -15,7 +15,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(hello());
     }
-    
+    private static String hello() {
+        return "Bonjour sur GitHub";
+    }
 }

e64555cec713d063b1ff32857b890e0a32814793
chaillou <chaillou@hp60.dev.afpa>
2019-03-25 15:00:30 +0100

    add "City" class, with attribute and constructor

---
+++ src/entity/City.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author chaillou
+ */
+public class City {
+    
+    private String _name;
+    private int _nbOfLiving;
+    private String _country;
+
+    public City(String name, int nbOfLiving, String country) {
+        this._name = name;
+        this._nbOfLiving = nbOfLiving;
+        this._country = country;
+    }
+    
+    @Override
+    public String toString () {
+        return "" + _name + " is a city with " + _nbOfLiving + "people in " + _country ; 
+    }
+    
+}
